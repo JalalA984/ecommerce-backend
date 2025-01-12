47b4ca08c3d78ef45a15fdd43c371046841ff519
Jalal <128756237+JalalA984@users.noreply.github.com>
2025-01-12 15:20:10 -0500

    first commit, setup project properties

---
+++ src/test/java/io/github/jalala984/ecommerce_backend/EcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jalala984.ecommerce_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
